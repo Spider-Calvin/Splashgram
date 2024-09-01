228ddce40b6b03eca5a9c9c60be54546724cef23
spider-calvin <calvindas7@gmail.com>
2024-09-02 00:57:56 +0530

    routing setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
+import { Routes, Route } from "react-router-dom";
+
+import { Home } from "./root";
+import SignIn from "./auth/forms/sign_in";
+import SignUp from "./auth/forms/sign_up";
+import AuthLayout from "./auth/auth_layout";
+
 function App() {
   return (
-    <>
-      <div>app</div>
-    </>
+    <main className="flex h-screen">
+      <Routes>
+        {/* public routes */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+        </Route>
+        {/* Private routes */}
+
+        <Route index element={<Home />} />
+      </Routes>
+    </main>
   );
 }
 
