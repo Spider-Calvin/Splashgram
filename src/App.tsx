010723b9b836efc8918788dfe9ed1c8f4cdfbbb9
spider-calvin <calvindas7@gmail.com>
2024-09-04 20:59:23 +0530

    root layout home created

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 
-import { Home } from "./root";
+import {
+  Home,
+  RootLayout,
+  Explore,
+  Saved,
+  CreatePost,
+  Profile,
+  EditPost,
+  PostDetails,
+  UpdateProfile,
+  AllUsers,
+} from "@/root";
 import SignIn from "./auth/forms/sign_in";
 import SignUp from "./auth/forms/sign_up";
 import AuthLayout from "./auth/auth_layout";
@@ -17,7 +28,17 @@ function App() {
         </Route>
         {/* Private routes */}
 
-        <Route index element={<Home />} />
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/saved" element={<Saved />} />
+          <Route path="/all-users" element={<AllUsers />} />
+          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/update-post/:id" element={<EditPost />} />
+          <Route path="/posts/:id" element={<PostDetails />} />
+          <Route path="/profile/:id/*" element={<Profile />} />
+          <Route path="/update-profile/:id" element={<UpdateProfile />} />
+        </Route>
       </Routes>
       <Toaster />
     </main>
--- src/root/root-layout.tsx
+++ src/root/root-layout.tsx
@@ -1,5 +1,19 @@
+import BottomBar from "@/components/shared/bottombar";
+import Sidebar from "@/components/shared/sidebar";
+import Topbar from "@/components/shared/topbar";
+import { Outlet } from "react-router-dom";
+
 function RootLayout() {
-  return <div>RootLayout</div>;
+  return (
+    <div className="w-full md:flex">
+      <Topbar />
+      <Sidebar />
+      <section className="flex flex-1 h-full">
+        <Outlet />
+      </section>
+      <BottomBar />
+    </div>
+  );
 }
 
 export default RootLayout;
